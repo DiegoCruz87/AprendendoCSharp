35a1320a0b74ae990982a815270399b72ef8833c
DiegoCruz87 <80112599+DiegoCruz87@users.noreply.github.com>
2023-04-18 21:32:51 -0300

    Projeto
    C#

---
+++ 1-OlaMundo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ola, mundo");
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
+
+
+
---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 5 - Caracteres e Texto");
+
+        char letra = 'a';
+        Console.WriteLine(letra); 
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        letra = (char)(86 + 1);
+        Console.WriteLine(letra);
+
+        string primeiraFrase = "Alura - Cursos de Tecnologia ";
+        Console.WriteLine(primeiraFrase + 2022);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
+ 
+
